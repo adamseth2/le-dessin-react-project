9f970989c8dd26ab5acc0b3d4feee87fc5218dcd
adamseth2 <35408139+adamseth2@users.noreply.github.com>
2020-09-03 14:24:31 -0700

    make sure Kanye Quote is Appropriate

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,25 +3,41 @@ import { title } from '../scss/Title.module.scss';
 
 function Title() {
   const [kanyeWestQuote, setKanyeWestQuote] = useState(null);
+  const [appropriateQuote, setAppropriateQuote] = useState(false);
   const kanyeWestApi = () => {
-    let notAppropriate;
     fetch('https://api.kanye.rest')
       .then(res => res.json())
-      .then(data => {
-        setKanyeWestQuote(data.quote);
-        console.log('bobbbbb');
-      })
+      .then(data => setKanyeWestQuote(data.quote))
       .then(() => console.log(kanyeWestQuote));
   };
   useEffect(() => {
-    let kanyeWestQuoteNotAppropriate;
-    while (kanyeWestQuoteNotAppropriate) {
-      // kanyeWest Api
-    }
+    kanyeWestApi();
   }, []);
+  const checkIfApporitate = () => {
+    const inappropriateWords = ['porn', 'fuck', 'shit', 'f#%k', 'sex'];
+    let kanyeWestQuoteArray = kanyeWestQuote.split(' ');
+    console.log(kanyeWestQuoteArray);
+    for (let inappropriateWord of inappropriateWords) {
+      console.log(inappropriateWord);
+      console.log(kanyeWestQuoteArray.indexOf(inappropriateWord));
+      if (kanyeWestQuoteArray.indexOf(inappropriateWord) !== -1) {
+        return false;
+      }
+    }
+    return true;
+  };
+  useEffect(() => {
+    console.log('bobbbdadadadadadwe');
+    if (kanyeWestQuote && !checkIfApporitate()) {
+      console.log('this quote is not apporiate');
+      kanyeWestApi();
+    } else setAppropriateQuote(true);
+  }, [kanyeWestQuote]);
   return (
     <div className={title}>
-      <h2 className='kanye-west-quote'>{kanyeWestQuote}</h2>
+      {appropriateQuote && (
+        <h2 className='kanye-west-quote'>{kanyeWestQuote}</h2>
+      )}
       <p>-Kanye West</p>
     </div>
   );
